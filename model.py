44e12a933537b5ab49cb67a1a504adf4561c6293
Eercee <148677038+Eercee@users.noreply.github.com>
2024-06-01 12:15:14 +0300

    Update model.py

--- model.py
+++ model.py
@@ -1,4 +1,3 @@
-# EE beated it
 import os
 import numpy as np
 import tensorflow as tf
@@ -6,90 +5,81 @@ from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
 from tensorflow.keras.optimizers import Adam
 
+#experimental
 
-#file_path = os.path.join(dest, "combined_data.npy") bunu değiştir path'e göre
+os.path.exists(fp):
+    cd = np.load(fp, allow_pickle=True).item()
+    print("u.")
 
+if 'cd' in locals():
+    sd = cd['sd']
+    sl = cd['sl']
 
-os.path.exists(file_path):
-    combined_data = np.load(file_path, allow_pickle=True).item()
-    print("uploaded.")
+    A1, B2 = [], []
+    C3, D4 = [], []
+    E5, F6 = [], []
 
-if 'combined_data' in locals():
-    separated_data = combined_data['separated_data']
-    separated_labels = combined_data['separated_labels']
+    for X7, Y8 in zip(sd, sl):
+        Z9 = len(X7)
+        T10 = int(0.7 * Z9)
+        V11 = int(0.15 * Z9)
 
-    train_data, train_labels = [], []
-    val_data, val_labels = [], []
-    test_data, test_labels = [], []
+        A1.extend(X7[:T10])
+        B2.extend(Y8[:T10])
+        C3.extend(X7[T10:T10 + V11])
+        D4.extend(Y8[T10:T10 + V11])
+        E5.extend(X7[T10 + V11:])
+        F6.extend(Y8[T10 + V11:])
 
-    for data, labels in zip(separated_data, separated_labels):
-        num_samples = len(data)
-        num_train = int(0.7 * num_samples)
-        num_val = int(0.15 * num_samples)
+    A1 = np.array(A1)
+    B2 = np.array(B2)
+    C3 = np.array(C3)
+    D4 = np.array(D4)
+    E5 = np.array(E5)
+    F6 = np.array(F6)
 
-        train_data.extend(data[:num_train])
-        train_labels.extend(labels[:num_train])
-        val_data.extend(data[num_train:num_train + num_val])
-        val_labels.extend(labels[num_train:num_train + num_val])
-        test_data.extend(data[num_train + num_val:])
-        test_labels.extend(labels[num_train + num_val:])
-
-    train_data = np.array(train_data)
-    train_labels = np.array(train_labels)
-    val_data = np.array(val_data)
-    val_labels = np.array(val_labels)
-    test_data = np.array(test_data)
-    test_labels = np.array(test_labels)
-   
-    
-    class ImageModel:
+    class X:
 
         def __init__(self):
-            gpus = tf.config.experimental.list_physical_devices('GPU')
-            for gpu in gpus:
-                tf.config.experimental.set_memory_growth(gpu, True)
-            self.model = None
+            g = tf.config.experimental.list_physical_devices('GPU')
+            for h in g:
+                tf.config.experimental.set_memory_growth(h, True)
+            self.i = None
 
-        def build(self):
-            self.model = Sequential()
-            self.model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', padding='same', input_shape=(100, 100, 3)))
-            self.model.add(MaxPooling2D(pool_size=(2, 2)))
-            self.model.add(Conv2D(64, kernel_size=(3, 3), activation='relu', padding='same'))
-            self.model.add(MaxPooling2D(pool_size=(2, 2)))
-            self.model.add(Conv2D(128, kernel_size=(3, 3), activation='relu', padding='same'))
-            self.model.add(MaxPooling2D(pool_size=(2, 2)))
-            self.model.add(Conv2D(256, kernel_size=(3, 3), activation='relu', padding='same'))
-            self.model.add(MaxPooling2D(pool_size=(2, 2)))
-            self.model.add(Flatten())
-            self.model.add(Dense(1024, activation='relu'))
-            self.model.add(Dropout(0.5))  # Dropoutlu daha iyi
-            self.model.add(Dense(4, activation='softmax'))
+        def J12(self):
+            self.i = Sequential([
+                Conv2D(23, kernel_size=(4, 4), activation='tanh', padding='valid', input_shape=(120, 120, 4)),
+                MaxPooling2D(pool_size=(3, 3)),
+                Conv2D(46, (4, 4), activation='tanh', padding='valid'),
+                MaxPooling2D(pool_size=(3, 3)),
+                Conv2D(92, (4, 4), activation='tanh', padding='valid'),
+                MaxPooling2D(pool_size=(3, 3)),
+                Conv2D(184, (4, 4), activation='tanh', padding='valid'),
+                MaxPooling2D(pool_size=(3, 3)),
+                Flatten(),
+                Dense(512, activation='tanh'),
+                Dense(5, activation='sigmoid')
+            ])
 
-        def summary(self):
-            self.model.summary()
+        def K13(self):
+            self.i.summary()
 
-        def compile(self):
-            self.model.compile(optimizer=Adam(learning_rate=0.0001), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+        def L14(self):
+            self.i.compile(optimizer=Adam(learning_rate=0.0005), loss='categorical_crossentropy', metrics=['accuracy'])
 
-        def fit(self, train_data, train_labels, val_data, val_labels):
-            
-            self.model.fit(train_data, train_labels, validation_data=(val_data, val_labels), epochs=30, verbose=2)
-          
-            #model_save_path = ''
-            self.model.save(model_save_path)
-            print(f"Model saved to {model_save_path}")
+        def M15(self, A1, B2, C3, D4):
+            self.i.fit(A1, B2, validation_data=(C3, D4), epochs=40, verbose=2)
+            self.i.save(ms)
+            print(f"s {ms}")
 
+        def N16(self, E5):
+            return self.i.predict(E5)
 
-        def predict(self, test_data):
-            return self.model.predict(test_data)
+    O17 = X()
+    O17.J12()
+    O17.L14()
+    O17.K13()
+    O17.M15(A1, B2, C3, D4)
 
-    model = ImageModel()
-    model.build()
-    model.compile()
-    model.summary()
-    model.fit(train_data, train_labels, val_data, val_labels)
-
-    
-    loss, accuracy = model.model.evaluate(test_data, test_labels, verbose=2)
-    print(f'Test accuracy: {accuracy * 100:.2f}%')
-
+    P18, Q19 = O17.i.evaluate(E5, F6, verbose=2)
+    print(f'{Q19 * 100:.2f}%')
